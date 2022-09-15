7792239a929b61cf54871032ba77ff8d7174f205
Pedro Bernardo <pedroh.vicent@gmail.com>
2022-09-15 11:55:29 -0300

    Commitando Projeto base

---
+++ src/main/java/com/exacta/exactaproject/ExactaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.exacta.exactaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExactaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExactaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exacta/exactaproject/ExactaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exacta.exactaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExactaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
